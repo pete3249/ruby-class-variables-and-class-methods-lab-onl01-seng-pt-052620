1665631f3015fa2e563e16fac8a19b9a546c8d08
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-15 21:04:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   def self.genre_count
     @@genres.map do |genre|
       binding.pry
-      if @@genres.include?("genre")
+      if @@genres.include?(genre)
         
       else
         
