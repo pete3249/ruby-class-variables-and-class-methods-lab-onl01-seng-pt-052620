7ab18711b0ce24fd64ca91f5499579ed530d0fbc
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-15 20:55:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   attr_accessor :name, :artist, :genre 
   @@count = 0
-  @@genres = []
+  @@genres = [""]
 
   
   def initialize(name, artist, genre)
@@ -11,6 +11,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << genre
   end
   
   def self.count
